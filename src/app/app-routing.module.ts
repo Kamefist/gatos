a9a6c14a2ce7f98722382c9498583250e6551228
andres <andrewsmv95@gmail.com>
2025-05-25 04:52:26 +0200

    Avance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { AdopcionesComponent } from './components/adopciones/adopciones.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'adopciones', component: AdopcionesComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'noticias', component: NoticiasComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AdopcionesComponent } from './components/adopciones/adopciones.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NoticiasComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    AdopcionesComponent,
+    GaleriaComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
